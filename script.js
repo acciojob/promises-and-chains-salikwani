36a46bf0e57cfe476d42c86fd6145c3f40f2268d
salikwani <120441251+salikwani@users.noreply.github.com>
2023-04-03 10:26:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,17 +3,15 @@ var nameElement = document.querySelector("#name");
 var ageElement = document.querySelector("#age");
 var buttonElement = document.querySelector("#btn");
 
-var nameVal = nameElement.value;
-var ageVal = ageElement.value;
-
-if(!(ageVal && nameVal)) {
-	alert("Please enter valid details!");
-	return;
-}
-
 var promise = new Promise(function(resolve, reject) {
 	setTimeout(function() {
+		var nameVal = nameElement.value;
+		var ageVal = ageElement.value;
 		
+		if(!(ageVal && nameVal)) {
+			alert("Please enter valid details!");
+			return;
+		}
 		if(ageVal < 18){
 			reject(`Oh sorry ${nameVal}. You aren't old enough.`);
 		} else {
