1da4f05315250d09813b1b551f79fbe212270f7a
salikwani <120441251+salikwani@users.noreply.github.com>
2023-04-03 10:09:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,3 +4,24 @@ var ageElement = document.querySelector("#age");
 var buttonElement = document.querySelector("#btn");
 
 buttonElement.addEventListener('click',sol);
+
+function sol() {
+	var nameVal = nameElement.value;
+	var ageVal = ageElement.value;
+
+	if(!(ageVal && nameVal)) {
+		alert("Please enter valid details!");
+		return;
+	}
+
+	var promise = new Promise(resolve, reject) {
+		setTimeout(function() {
+			if(ageVal < 18){
+				reject(`Oh sorry ${nameVal}. You aren't old enough.`);
+			} else {
+				resolve(`Welcome ${nameVal}. You can vote.`);
+			}
+		},4000);
+	}
+	return promise;
+}
