8d7bbce17e4a829aef6653c0b3484c85e50144e2
salikwani <120441251+salikwani@users.noreply.github.com>
2023-04-03 09:59:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+var nameElement = document.querySelector("#name");
+var ageElement = document.querySelector("#age");
+var buttonElement = document.querySelector("#btn");
+
+buttonElement.addEventListener('click',sol);
